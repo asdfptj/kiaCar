675671a7195b6b7a373d8185391e2dc0b1c8ec6d
asdftaejun <taejunb70@gmail.com>
2023-11-02 16:58:45 +0900

    [black] logo edit

---
+++ dist/asset/resource/js/scroll.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var header = document.getElementById('header');
+    if (window.scrollY >= 0.2 * window.innerHeight) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+};
